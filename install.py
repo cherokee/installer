3fbee9ca4f2b687bd8c8a0bf32f66cc9a523cb8f
alobbs <alvaro@alobbs.com>
2011-01-17 22:22:04 +0000

    Adds support for --from-<something> parameter. It also adds initial support for init.d and launchd.
    git-svn-id: svn://cherokee-project.com/installer@6124 5dc97367-97f1-0310-9951-d761b3857238

--- install.py
+++ install.py
@@ -27,12 +27,40 @@
 import re
 import os
 import sys
+import glob
 import subprocess
 
 BUILD_DIR          = "/var/tmp/cherokee-build"
 URL_LATEST_RELEASE = "http://www.cherokee-project.com/cherokee-latest-tarball"
 PREFIX             = "/opt/cherokee"
 
+PHASE_DOWNLOAD = 1
+PHASE_UNPACK   = 2
+PHASE_COMPILE  = 3
+PHASE_INITD    = 4
+PHASE_REPORT   = 5
+
+
+# Texts
+#
+LAUNCHD_PLIST = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key><string>com.octality.cherokee</string>
+  <key>RunAtLoad</key><true/>
+  <key>WorkingDirectory</key><string>%(PREFIX)s</string>
+  <key>ProgramArguments</key><array>
+       <string>sbin/cherokee</string>
+  </array>
+</dict>
+</plist>
+"""
+
+# Globals
+#
+start_at = PHASE_DOWNLOAD
 
 
 # ANSI Colors
@@ -116,18 +144,43 @@ def download (url, target_file):
     o = open (target_file, 'w+')
     o.write (i.read())
 
+def read_input (prompt):
+    try:
+        # Python 2.x
+        return raw_input (prompt)
+    except NameError:
+        # Python 3.x
+        return input (prompt)
+
+def read_yes_no (prompt, empty_is=None):
+    while True:
+        ret = read_input (prompt).lower()
+        if ret in ('y','yes'):
+            return True
+        if ret in ('n','no'):
+            return True
+        if not ret and empty_is != None:
+            return empty_is
+
 
 # Cherokee
 #
-def cherokee_download():
+def cherokee_download (tar_file):
     latest_local = os.path.join (BUILD_DIR, "cherokee-latest.tar.gz")
 
     rm (BUILD_DIR)
-    rm (latest_local)
+    rm (tar_file)
     mkdir (BUILD_DIR)
 
-    download (URL_LATEST_RELEASE, latest_local)
-    return latest_local
+    download (URL_LATEST_RELEASE, tar_file)
+
+
+def cherokee_find_unpacked():
+    for f in os.listdir (BUILD_DIR):
+        fp = os.path.join (BUILD_DIR, f)
+        tmp = re.findall (r'cherokee-(\d+\.\d+\.\d+)', f)
+        if tmp and os.path.isdir (fp):
+            return fp
 
 
 def cherokee_unpack (latest_local):
@@ -135,11 +188,7 @@ def cherokee_unpack (latest_local):
     exe ("gzip -dc '%s' | tar xfv -" %(latest_local), cd=BUILD_DIR)
 
     # Look for the src directory
-    for f in os.listdir (BUILD_DIR):
-        fp = os.path.join (BUILD_DIR, f)
-        tmp = re.findall (r'cherokee-(\d+\.\d+\.\d+)', f)
-        if tmp and os.path.isdir (fp):
-            return fp
+    return cherokee_find_unpacked()
 
 
 def cherokee_compile (src_dir):
@@ -152,24 +201,77 @@ def cherokee_compile (src_dir):
         exe ("sudo make install", cd=src_dir)
 
 
+def cherokee_set_initd():
+    print('')
+
+    proceed = read_yes_no ("Do you want Cherokee to be started at boot time? [Y/n] ", True)
+    if not proceed:
+        return
+
+    vars = globals()
+    vars.update (locals())
+
+    if sys.platform == 'darwin':
+        plist_fd = os.path.join (PREFIX, "launchd-cherokee.plist")
+
+        # Write the plist file
+        txt = LAUNCHD_PLIST %(vars)
+        f = open (plist_fd, 'w+')
+        f.write (txt)
+        f.close()
+
+        # Let launchd know about it
+        exe ("launchctl load '%s'" %(plist_fd))
+        exe ("launchctl start cherokee")
+
+
 def cherokee_report():
     cherokee_fp = os.path.join (PREFIX, "sbin", "cherokee")
     exe ("%s -i" %(cherokee_fp), green)
 
 
+
 # Main
 #
 def main():
-    tar_file = cherokee_download ()
-    src_dir  = cherokee_unpack (tar_file)
-    cherokee_compile (src_dir)
-    cherokee_report ()
+    tar_file = os.path.join (BUILD_DIR, "cherokee-latest.tar.gz")
+
+    if start_at <= PHASE_DOWNLOAD:
+        cherokee_download (tar_file)
+
+    if start_at <= PHASE_UNPACK:
+        src_dir = cherokee_unpack (tar_file)
+    else:
+        src_dir = cherokee_find_unpacked()
+
+    if start_at <= PHASE_COMPILE:
+        cherokee_compile (src_dir)
+
+    if start_at <= PHASE_INITD:
+        cherokee_set_initd()
+
+    if start_at <= PHASE_REPORT:
+        cherokee_report ()
 
 
 def check_prerequisites():
     assert which ("make"), "Make is required for the compilation"
 
 
+def process_parameters():
+    global start_at
+
+    if '--from-unpack' in sys.argv:
+        start_at = PHASE_UNPACK
+    if '--from-compile' in sys.argv:
+        start_at = PHASE_COMPILE
+    if '--from-initd' in sys.argv:
+        start_at = PHASE_INITD
+    if '--from-report' in sys.argv:
+        start_at = PHASE_REPORT
+
+
 if __name__ == '__main__':
+    process_parameters()
     check_prerequisites()
     main()
